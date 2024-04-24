655bf840393bcc234c8c8dbcee458514cda2d5f9
Antaresx03 <jorgem271803@gmail.com>
2024-04-23 19:10:00 -0500

    models added

---
+++ my_app/tasks/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class User(models.Model):
+    username = models.CharField(max_length=200)
+    email = models.TextField()
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+
+    def str(self):
+        return f"{self.username} {self.email} {self.first_name} {self.last_name}"
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def str(self):
+        return f"{self.name} {self.description}"
