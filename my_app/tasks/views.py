811707f465f890d7ff61fbb8b1e284392f4da5f2
Duban rene sanchez trochez <dubanrene2662@gmail.com>
2024-04-23 20:40:52 -0500

    Views added

--- my_app/tasks/views.py
+++ my_app/tasks/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import JsonResponse
+from django.core.serializers import serialize
+
+from .models import Task, User
+
+def index(request):
+    tasks = Task.objects.all()
+    data = serialize('python', tasks)
+    return JsonResponse(data, safe=False)
+
+def users(request):
+    users = User.objects.all()
+    data = serialize('python', users)
+    return JsonResponse(data, safe=False)
